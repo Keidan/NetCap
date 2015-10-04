39e7331c28fdcc82f606fd466ca096afd84f9c2d
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2015-10-04 11:11:10 +0200

    DEV+UI: Add payload viewer

---
+++ src/org/kei/android/phone/netcap/PayloadViewerActivity.java
@@ -0,0 +1,56 @@
+package org.kei.android.phone.netcap;
+
+import org.kei.android.phone.jni.net.NetworkHelper;
+import org.kei.android.phone.jni.net.layer.Payload;
+import org.kei.android.phone.netcap.utils.fx.Fx;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ *******************************************************************************
+ * @file PayloadViewerActivity.java
+ * @author Keidan
+ * @date 01/10/2015
+ * @par Project
+ * NetCap
+ *
+ * @par 
+ * Copyright 2015 Keidan, all right reserved
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY.
+ *
+ * License summary : 
+ *    You can modify and redistribute the sources code and binaries.
+ *    You can send me the bug-fix
+ *
+ * Term of the license in in the file license.txt.
+ *
+ *******************************************************************************
+ */
+public class PayloadViewerActivity extends Activity {
+
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_payloadviewer);
+    Fx.updateTransition(this, true);
+    setTitle(getResources().getText(R.string.app_name) + " - " + ApplicationCtx.getAppId(this));
+    ListView payloadLV = (ListView)findViewById(R.id.payloadLV);
+    Payload p = (Payload)ApplicationCtx.getAppLayer(this);
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 
+        R.layout.listview_simple_row, R.id.label1, 
+        NetworkHelper.formatBuffer(p.getDatas()));
+    payloadLV.setAdapter(adapter); 
+  }
+
+  @Override
+  public void onBackPressed() {
+    super.onBackPressed();
+    Fx.updateTransition(this, false);
+    finish();
+  }
+}
